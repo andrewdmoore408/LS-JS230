aa00cf0407c0696677f650ea9bae5695324b088c
Andrew Moore <andrewdmoore84@gmail.com>
2022-02-25 15:40:42 -0500

    Extract logic out into functions (inspiration taken from LS solution); remove comments

--- lesson_5/photo_gallery2/public/javascripts/gallery.js
+++ lesson_5/photo_gallery2/public/javascripts/gallery.js
@@ -1,24 +1,3 @@
-/*
-Step 1: Fetch Photos Data and Render Photos on Page Load
-
-For this step, we're going to issue an AJAX request to the server to get the JSON data for all the photos on page load, then use that to:
-
-    Render the photos template, and write it to the #slides div
-    Render the photo_information template using the first photo's data, and write it to section > header element that needs to contain the photo information
-
-The API end point that we need to use for this step:
-
-    Path: /photos
-    HTTP method: GET
-    Returns: an array of photos data in JSON format
-
-Since this is an HTTP GET, feel free to type http://localhost:3000/photos in your browser to see the data that it returns. If you're stuck or need a hint, check out the solution below.
-
-A couple of hints on how to approach the implementation:
-
-    Spend some time looking through the Handlebars templates to understand the data that needs to be supplied to each
-    Using your browser, make some calls to the various API endpoints. Use the DevTools network tab to see what json data is returned
-*/
 document.addEventListener('DOMContentLoaded', () => {
   const Photo = {
     init({ src, title, created_at, caption, likes, favorites, id }) {
@@ -34,16 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  function setUpHandlebarsTemplatesPartials() {
+    [...document.querySelectorAll("script[type='text/x-handlebars']")].filter(element => !element.dataset.type).forEach(tmpl => {
+      templates[tmpl.id] = Handlebars.compile(tmpl.innerHTML);
+    });
+
+    [...document.querySelectorAll('script[type="text/x-handlebars"]')].filter(element => element.dataset.type === 'partial').forEach(tmpl => {
+      templates[tmpl.id] = Handlebars.registerPartial(tmpl.id, tmpl.innerHTML);
+    });
+  }
+
+  function renderPhotos() {
+    document.querySelector('#slides').innerHTML = templates.photos({ photos: photos });
+  }
+
+  function renderPhotoInfo(photo) {
+    document.querySelector('section > header').innerHTML = templates.photo_information(photo);
+  }
+
+  function renderPhotoComments(commentsArray) {
+    document.querySelector('#comments > ul').innerHTML = templates.photo_comments({ comments: commentsArray });
+  }
+
   const photos = [];
   const templates = {};
 
-  [...document.querySelectorAll("script[type='text/x-handlebars']")].filter(element => !element.dataset.type).forEach(tmpl => {
-    templates[tmpl.id] = Handlebars.compile(tmpl.innerHTML);
-  });
-
-  [...document.querySelectorAll('script[type="text/x-handlebars"]')].filter(element => element.dataset.type === 'partial').forEach(tmpl => {
-    templates[tmpl.id] = Handlebars.registerPartial(tmpl.id, tmpl.innerHTML);
-  });
+  setUpHandlebarsTemplatesPartials();
 
   fetch('/photos')
     .then(response => response.json())
@@ -52,14 +47,11 @@ document.addEventListener('DOMContentLoaded', () => {
         photos.push(Object.create(Photo).init(photo));
       });
 
-      document.querySelector('#slides').innerHTML = templates.photos({photos: photos});
-
-      document.querySelector('section > header').innerHTML = templates.photo_information(photos[0]);
+      renderPhotos();
+      renderPhotoInfo(photos[0]);
 
       fetch(`comments?photo_id=${photos[0].id}`)
         .then(response => response.json())
-        .then(commentsArray => {
-          document.querySelector('#comments > ul').innerHTML = templates.photo_comments({comments: commentsArray});
-        });
+        .then(commentsArray => renderPhotoComments(commentsArray));
     });
 });
