9b8bc55ea931040a0ec3edc3ae27068990ce00b5
Andrew Moore <andrewdmoore84@gmail.com>
2022-02-25 23:16:39 -0500

    Add previous and next photo slideshow functionality; update placeholder image sources to use local images

--- lesson_5/photo_gallery2/public/javascripts/gallery.js
+++ lesson_5/photo_gallery2/public/javascripts/gallery.js
@@ -13,6 +13,43 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  function attachSlideshowControls() {
+    document.querySelector('a.prev').addEventListener('click', prev);
+    document.querySelector('a.next').addEventListener('click', next);
+
+    function next(event) {
+      event.preventDefault();
+
+      let activeIndex = photos.findIndex(photo => photo === activePhoto);
+
+      activeIndex = (activeIndex === photos.length - 1) ? 0 : activeIndex += 1;
+
+      activePhoto = photos[activeIndex];
+
+      const activePhotoElement = document.querySelector(`#slides > figure[data-id="${activePhoto.id}"]`);
+      document.querySelector('#slides').insertAdjacentElement('afterbegin', activePhotoElement);
+
+      renderActivePhotoInfo();
+      renderActivePhotoComments();
+    }
+
+    function prev(event) {
+      event.preventDefault();
+
+      let activeIndex = photos.findIndex(photo => photo === activePhoto);
+      const precedingIndex = (activeIndex === 0) ? photos.length - 1 : activeIndex - 1;
+      const precedingPhoto = photos[precedingIndex];
+
+      const precedingPhotoElement = document.querySelector(`#slides > figure[data-id="${precedingPhoto.id}"]`);
+      document.querySelector('#slides').insertAdjacentElement('afterbegin', precedingPhotoElement);
+
+      activePhoto = precedingPhoto;
+
+      renderActivePhotoInfo();
+      renderActivePhotoComments();
+    }
+  }
+
   function setUpHandlebarsTemplatesPartials() {
     [...document.querySelectorAll("script[type='text/x-handlebars']")].filter(element => !element.dataset.type).forEach(tmpl => {
       templates[tmpl.id] = Handlebars.compile(tmpl.innerHTML);
@@ -24,35 +61,47 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function renderPhotos() {
-    document.querySelector('#slides').innerHTML = templates.photos({ photos: photos });
+    document.querySelector('#slides').innerHTML = templates.photos({photos: photos});
   }
 
-  function renderPhotoInfo(photo) {
-    document.querySelector('section > header').innerHTML = templates.photo_information(photo);
+  function renderActivePhotoInfo() {
+    document.querySelector('section > header').innerHTML = templates.photo_information(activePhoto);
   }
 
-  function renderPhotoComments(commentsArray) {
-    document.querySelector('#comments > ul').innerHTML = templates.photo_comments({ comments: commentsArray });
+  async function renderActivePhotoComments() {
+    const commentsArray = await getActivePhotoComments();
+
+    document.querySelector('#comments > ul').innerHTML = templates.photo_comments({comments: commentsArray});
+  }
+
+  async function getActivePhotoComments() {
+    return await fetch(`comments?photo_id=${activePhoto.id}`)
+                                  .then(response => response.json());
   }
 
   const photos = [];
   const templates = {};
+  let activePhoto;
 
+  attachSlideshowControls();
   setUpHandlebarsTemplatesPartials();
 
   fetch('/photos')
     .then(response => response.json())
     .then(photosArray => {
-      photosArray.forEach(photo => {
-        photo.src = photo.src.replace('placehold.it', 'via.placeholder.com');
+      const localPhotoFilenames = ['./images/cows_on_road_country.jpg', './images/sete_cidades_daylight.jpg', './images/sete_cidades_sunset.jpg'];
+
+      photosArray.forEach((photo, index) => {
+        // use local photos
+        photo.src = localPhotoFilenames[index];
+        // photo.src = photo.src.replace('placehold.it', 'via.placeholder.com');
         photos.push(Object.create(Photo).init(photo));
       });
 
-      renderPhotos();
-      renderPhotoInfo(photos[0]);
+      activePhoto = photos[0];
 
-      fetch(`comments?photo_id=${photos[0].id}`)
-        .then(response => response.json())
-        .then(commentsArray => renderPhotoComments(commentsArray));
+      renderPhotos();
+      renderActivePhotoInfo();
+      renderActivePhotoComments();
     });
 });
